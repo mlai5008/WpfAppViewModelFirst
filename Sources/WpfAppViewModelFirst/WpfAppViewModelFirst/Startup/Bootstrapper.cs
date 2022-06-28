798dfc0697b6aeae50d1c82c234734e6a70b61ed
Юрий Харитончик <haritonchik-yn@adani.by>
2022-06-28 13:08:11 +0300

    Refact mian project

---
+++ Sources/WpfAppViewModelFirst/WpfAppViewModelFirst/Startup/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+
+namespace WpfAppViewModelFirst.Startup
+{
+    public class Bootstrapper
+    {
+        #region Consts
+        private const string ViewModelsAssembly = "WpfAppUseDataTemplateWithOutDataContext.ViewModels.dll";
+        private const string ViewsAssembly = "WpfAppUseDataTemplateWithOutDataContext.Views.dll";
+        #endregion
+
+        #region Methods
+        public IContainer Bootstrap()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            // Работает
+            //builder.RegisterModule(new ViewModelsModule());
+
+            // Работает
+            //******************************************
+            //builder.RegisterAssemblyModules(Assembly.LoadFrom("WpfAppWithOutDataContext.ViewModels.dll"));
+            //**************************************
+
+            //RegisterWindows(builder);
+            //RegisterViewModels(builder);
+            RegisterAssemblyModules(builder);
+            return builder.Build();
+        }
+
+        private void RegisterAssemblyModules(ContainerBuilder builder)
+        {
+
+        } 
+        #endregion
+    }
+}
